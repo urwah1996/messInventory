de53a4d5fc10f2d347e7f577b0b752e5a778e7a8
urwah1996 <mohammadurwah@gmail.com>
2017-02-03 18:35:26 +0500

    Update dbAccess.js

--- src/dbAccess.js
+++ src/dbAccess.js
@@ -1,60 +1,51 @@
 var Sequelize = require('sequelize');
+var dbTables = require('./dbTables');
+/*
+sequelize=database.sequelize;
+User=database.User;
+*/
 
-var sequelize = new Sequelize('centralMessInventory', 'sa', '1234' , {
-			dialect: 'mssql',
-			host: 'localhost',
-			port: 1433, // Default port
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            },
-     // omitNull: true ,
-			dialectOptions: {
-				instanceName: 'MSSQLSERVER'
-			}
-});
 
-var User = sequelize.define('user', {
-  username: {
-    type:Sequelize.STRING,
-    
-  },
-  password:{
-    type:Sequelize.STRING,
-  }
+function dbaccess() {
+  console.log("in .then");
+  var array = "newuser";
+  var object = [];
+  sequelize.sync().then(function () {
+    return User
+      .findOrCreate({ where: { username: array, password: 'hello' } })
+      .spread(function (user, created) {
 
-}
-);
-var array="newuser";
-var object=[];
-sequelize.sync().then(function() {
-return User
-      .findOrCreate({where: {username: array , password: 'hello'}})
-      .spread(function(user, created) {
-        /*console.log(user.get({
-          plain: true
-        }))*/
-        
-       
         console.log(created)
       });
-  
-});
 
-User.findOne({where:{username: 'world'}}).then(function(user){
-  if(user)
-     {
-       object=user.get({
-          plain: true
-        }) ;
-     } 
-     else{
-       console.log("Data not found");
-     }
-  }).then(function(){
-  console.log(object.password);
-  console.log("hellop")
-})
+  });
 
-  
+  User.findOne({ where: { username: 'world' } }).then(function (user) {
+    if (user) {
+      object = user.get({
+        plain: true
+      });
+    }
+    else {
+      console.log("Data not found");
+    }
+  }).then(function () {
+    console.log(object.password);
+    console.log("hellop")
+  })
+
+
+
+}
+
+
+sequelize = dbTables.sequelize;
+User = dbTables.User;
+paymentVoucher = dbTables.paymentVoucher;
+purchaceOrder = dbTables.purcahseOrder;
+purcahseOrderItems = dbTables.purcahseOrderItems;
+supplier = dbTables.supplier;
+foodItem = dbTables.foodItem;
+typeOfFood = dbTables.typeOfFood;
+
+dbaccess();
